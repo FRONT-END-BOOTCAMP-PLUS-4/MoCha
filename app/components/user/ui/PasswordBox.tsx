c8616adfed32aacc5a3983e67e916b5a278f03ee
junik <wkdwnsdlr37@gmail.com>
2025-04-20 15:28:39 +0900

    style: PasswoardBox 생성

---
+++ app/components/user/ui/PasswordBox.tsx
@@ -0,0 +1,61 @@
+import { useState } from 'react';
+import { Button } from '@/app/shared/ui/button/Button';
+import { useFullModal } from '@/app/components/user/store/useFullModalStore';
+
+export default function PasswoardBox() {
+  const close = useFullModal((state) => state.action.close);
+  const [next, setNext] = useState(false);
+  const toggleNext = () => setNext(!next)
+  return (
+    <div className="border-main bg-main-bg flex flex-col gap-3 rounded-lg border p-3">
+      {next ? (
+        <>
+          <div className="text-gray-6">
+            <label>새로운비밀번호:</label>
+            <input
+              type="password"
+              className="border-gray-5 grow border-b outline-none"
+              defaultValue={'새로운비밀번호'}
+            />
+          </div>
+          <div className="text-gray-6">
+            <label>새로운비밀번호 확인:</label>
+            <input
+              type="password"
+              className="border-gray-5 grow border-b outline-none"
+              defaultValue={'새로운비밀번호'}
+            />
+          </div>
+
+          <div className="flex justify-end gap-3">
+            <Button intent="ghost" className="hover:bg-main px-4 py-2" onClick={toggleNext}>
+              변경
+            </Button>
+            <Button intent="ghost" className="hover:bg-main px-4 py-2" onClick={close}>
+              취소
+            </Button>
+          </div>
+        </>
+      ) : (
+        <>
+          <div className="text-gray-6">
+            <label>기존비밀번호:</label>
+            <input
+              type="password"
+              className="border-gray-5 grow border-b outline-none"
+              defaultValue={'새로운비밀번호'}
+            />
+          </div>
+          <div className="flex justify-end gap-3">
+            <Button intent="ghost" className="hover:bg-main px-4 py-2" onClick={toggleNext}>
+              다음
+            </Button>
+            <Button intent="ghost" className="hover:bg-main px-4 py-2" onClick={close}>
+              취소
+            </Button>
+          </div>
+        </>
+      )}
+    </div>
+  );
+}
