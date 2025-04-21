908819eead03bd0211fecae660855150d4b70423
Yoonstar <yagobo1110@naver.com>
2025-04-21 22:00:25 +0900

    feat: user entity 변경

--- domain/user/entities/User.ts
+++ domain/user/entities/User.ts
@@ -4,6 +4,21 @@ export class User {
     public readonly email: string,
     public readonly nickname: string | null,
     public readonly phone_number: string | null,
-    public readonly provider: string
+    public readonly provider: number,
+    public readonly password?: string,
+    public readonly deleted_at?: string
   ) {}
+
+  // Supabase에서 가져온 plain object를 클래스로 변환해주는 정적 메서드
+  static from(data: any): User {
+    return new User(
+      data.id,
+      data.email,
+      data.nickname,
+      data.phone_number,
+      data.provider,
+      data.password,
+      data.deleted_at
+    );
+  }
 }
