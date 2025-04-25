7a7a420db4889a1c7f21930d512dd1e849c7aa8f
Yoonstar <yagobo1110@naver.com>
2025-04-25 20:24:41 +0900

    feat: 회원가입 로직 수정

--- application/usecases/user/dto/SignupDto.ts
+++ application/usecases/user/dto/SignupDto.ts
@@ -1,9 +1,9 @@
 export class SignupRequestDto {
   constructor(
     public email: string,
-    public password: string,
-    public nickname: string,
-    public phone_number: string,
+    public password: string | null,
+    public nickname: string | null,
+    public phone_number: string | null,
     public provider: number
   ) {}
 }
