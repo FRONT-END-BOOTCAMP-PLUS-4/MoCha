607cfd853dc85ce7831fe7bec7cc35268f301630
junik <wkdwnsdlr37@gmail.com>
2025-04-27 07:33:20 +0900

    Refactor: CategoryRepo 파라미터 이름수정

--- domain/repositories/CategoryRepo.ts
+++ domain/repositories/CategoryRepo.ts
@@ -1,5 +1,5 @@
 export interface CategoryRepo {
-    GETmonthlyCategory(props: {userId: string, start:string, end:string}): Promise<{
+    GETmonthlyCategory(props: {userId: string, startDate:string, endDate:string}): Promise<{
       [K in 'incomes' | 'expenses']: Array<{
         is_expense: boolean;
         amount: number;
