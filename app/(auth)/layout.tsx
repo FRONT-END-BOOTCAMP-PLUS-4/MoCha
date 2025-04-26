6040be286b46379f642622517e751cb5950c28e6
Yoonstar <yagobo1110@naver.com>
2025-04-26 17:08:37 +0900

    feat: 로그인 상태일 경우, 로그인/회원가입 페이지 접근 차단

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,9 +1,30 @@
 'use client';
 
-export default function AuthLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+import { ReactNode, useEffect, useState } from 'react';
+
+import { Loader2 } from 'lucide-react';
+import { useRouter } from 'next/navigation';
+
+export default function AuthLayout({ children }: { children: ReactNode }) {
+  const router = useRouter();
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    const token = localStorage.getItem('access_token');
+    if (token) {
+      router.push('/');
+    } else {
+      setIsLoading(true);
+    }
+  }, [router]);
+
+  if (!isLoading) {
+    return (
+      <div className="flex h-screen items-center justify-center">
+        <Loader2 className="text-main h-8 w-8 animate-spin" />
+      </div>
+    );
+  }
+
   return <div className="bg-main-bg m-auto max-w-(--layout-w-auth)">{children}</div>;
 }
