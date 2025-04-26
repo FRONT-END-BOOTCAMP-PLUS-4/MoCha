a33dc5382584b5bef88d7c4ae0c8955a50a5a0c5
imi21123 <imi21123@naver.com>
2025-04-27 03:44:17 +0900

    feat: 특정 날짜 상세 내역 조회 api 구현

---
+++ app/api/transactions/route.ts
@@ -0,0 +1,23 @@
+import { GetDailyDetailUsecase } from '@/application/usecases/transactions/GetDailyDetailUsecase';
+import { SbTransactionRepo } from '@/infra/repositories/supabase/SbTransactionRepo';
+import { verifyAccessToken } from '@/infra/utils/jwt';
+import { NextRequest, NextResponse } from 'next/server';
+
+export async function GET(req: NextRequest) {
+  try {
+    const access_token = req.headers.get('authorization')?.replace('Bearer ', '');
+    if (!access_token) {
+      return NextResponse.json({ status: 401 });
+    }
+    const { user } = verifyAccessToken(access_token);
+    const id = user.id;
+    const date = req.nextUrl.searchParams.get('date');
+    const sbTransactionRepo = new SbTransactionRepo();
+    const dailyDetailUsecase = new GetDailyDetailUsecase(sbTransactionRepo);
+    const data = await dailyDetailUsecase.execute({ userId: id, date });
+    console.log('GET daily detail:', data);
+    return NextResponse.json({ status: 200, data });
+  } catch (err) {
+    return NextResponse.json({ status: 401 });
+  }
+}
--- infra/repositories/supabase/SbTransactionRepo.ts
+++ infra/repositories/supabase/SbTransactionRepo.ts
@@ -1,5 +1,6 @@
 import { TransactionRepo } from '@/domain/repositories/TransactionRepo';
 import { supabase } from '@/app/shared/lib/supabase';
+import { addDays } from 'date-fns';
 
 export class SbTransactionRepo implements TransactionRepo {
   async GETmonthlySummary(
@@ -35,6 +36,39 @@ export class SbTransactionRepo implements TransactionRepo {
         .gte('date', startDate)
         .lte('date', endDate);
 
+      return data ? data : [];
+    } catch (error) {
+      console.error('error:', error);
+      return [];
+    }
+  }
+
+  async GETdailyDetail(
+    userId: string,
+    date: string
+  ): Promise<
+    | {
+        id: number;
+        user_id: number;
+        category_id: number;
+        date: string;
+        amount: number;
+        memo: string | null;
+        is_expense: boolean;
+      }[]
+    | []
+  > {
+    try {
+      const startDate = new Date(date);
+      const endDate = addDays(startDate, 1);
+
+      const { data, error } = await supabase
+        .from('transaction')
+        .select(`id, user_id, category_id, date, amount, memo, is_expense`)
+        .eq('user_id', userId)
+        .gte('date', startDate.toISOString()) // 날짜 >= 2025-04-01T00:00:00
+        .lt('date', endDate.toISOString()); // 날짜 < 2025-04-02T00:00:00
+      console.log('data:', data);
       return data ? data : [];
     } catch (error) {
       console.error('error:', error);
