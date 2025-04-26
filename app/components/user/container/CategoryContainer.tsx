b292b067cd3878ab7238c81db4e5812f49f0b457
junik <wkdwnsdlr37@gmail.com>
2025-04-26 17:02:13 +0900

    feat: CategoryContainer fetch 추가

--- app/components/user/container/CategoryContainer.tsx
+++ app/components/user/container/CategoryContainer.tsx
@@ -1,53 +1,55 @@
-import { useEffect } from 'react';
-import CategoryList from '@/app/components/user/presentation/CategoryList';
-import CategoryPieChart from '@/app/components/user/presentation/CategoryPieChart';
-import { Category } from '@/app/shared/types/Chart';
-const data1: Category[] = [
-  { name: '급여', price: 500000, category: 'salary' },
-  { name: '투자수익', price: 500000, category: 'investment' },
-  { name: '용동', price: 500000, category: 'allowance' },
-  { name: '환급 & 환불', price: 500000, category: 'refund' },
-  { name: '기타', price: 500000, category: 'other' },
-];
+'use client';
+// package
+import { useEffect, useState } from 'react';
+// slice
+import Category from '../presentation/Category';
 
-const data2: Category[] = [
-  { name: '식비', price: 500000, category: 'food' },
-  { name: '생활', price: 500000, category: 'housing' },
-  { name: '교통', price: 500000, category: 'transportation' },
-  { name: '통신', price: 500000, category: 'communication' },
-  { name: '의료', price: 500000, category: 'medical' },
-  { name: '쇼핑', price: 500000, category: 'shopping' },
-  { name: '교육', price: 500000, category: 'education' },
-  { name: '문화 & 여가', price: 500000, category: 'culture' },
-  { name: '경조사', price: 500000, category: 'event' },
-  { name: '기타', price: 500000, category: 'other' },
-];
+type CategoryContainerProps = {
+  date: string;
+}
 
-export default function CategoryContainer() {
+type RawCategoryData = {
+  total_amount: number;
+  category_name: string;
+  category_primary_color: string;
+};
+
+export type CategoryItem = {
+      amount: number;
+      name: string;
+      color: string;
+};
+
+export default function CategoryContainer({date}: CategoryContainerProps) {
   
-  useEffect(()=>{
-    fetch('/api/category?date=2025-04',{
-      headers: { Authorization: `Bearer ${localStorage.getItem('access_token')}`},
-    })
-    .then( res => res.json())
-    .then( data => console.log(data))
-  },[])
+  const [incomes, setIncomes] = useState<CategoryItem[]>([]);
+  const [expenses, setExpenses] = useState<CategoryItem[]>([]);
 
-  return (
-    <div className="md:flex md:gap-3">
-      <div className="flex grow basis-0 flex-col items-center p-2">
-        <div className="size-40">
-          <CategoryPieChart categoryList={data1} />
-        </div>
-        <CategoryList categoryList={data1} />
-      </div>
+  useEffect(() => {
+    (async () => {
+      const res = await fetch(`/api/category?date=${date}`, {
+        headers: { Authorization: `Bearer ${localStorage.getItem('access_token')}` },
+      });
 
-      <div className="flex grow basis-0 flex-col items-center p-2">
-        <div className="size-40">
-          <CategoryPieChart categoryList={data2} />
-        </div>
-        <CategoryList categoryList={data2} />
-      </div>
-    </div>
-  );
+      const json = await res.json();
+      const incomesRaw: RawCategoryData[] = json.data.incomes;
+      const expensesRaw: RawCategoryData[] = json.data.expenses;
+
+      const incomesMapper: CategoryItem[] = (incomesRaw as RawCategoryData[]).map( value => ({
+        amount: value.total_amount,
+        name: value.category_name,
+        color: value.category_primary_color,
+      }));
+      const expensesMapper: CategoryItem[] = (expensesRaw as RawCategoryData[]).map( value => ({
+        amount: value.total_amount,
+        name: value.category_name,
+        color: value.category_primary_color,
+      }));
+
+      setIncomes(incomesMapper);
+      setExpenses(expensesMapper);
+    })();
+  }, [date]);
+
+  return <Category incomes={incomes} expenses={expenses}/>
 }
