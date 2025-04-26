bebd3e09fb682ce78e2bdcf0ac8e83d41433fb6d
Yoonstar <yagobo1110@naver.com>
2025-04-26 13:47:48 +0900

    refactor: 소셜 로그인 적용, provider type 수정

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -5,8 +5,9 @@ import { isValidEmail, isValidPassword } from '@/app/shared/consts/validation';
 
 import { Button } from '@/app/shared/ui/button';
 import { FormStatus } from '@/app/shared/types/FormStatus';
-import Image from 'next/image';
+import GoogleLoginButton from '@/app/components/auth/GoogleLoginButton';
 import Input from '@/app/shared/ui/input';
+import KakaoLoginButton from '@/app/components/auth/KakaoLoginButton';
 import Label from '@/app/shared/ui/label';
 import Link from 'next/link';
 import LogoImage from '@/app/components/auth/LogoImage';
@@ -16,7 +17,6 @@ import Title from '@/app/components/auth/Title';
 import { decodeJwt } from 'jose';
 import { getFieldMessage } from '@/app/shared/consts/errorMessages';
 import { useAuthStore } from '@/app/shared/stores/authStore';
-import { useGoogleLogin } from '@react-oauth/google';
 import useIsHide from '@/app/shared/hooks/useIsHide';
 import { useRouter } from 'next/navigation';
 
@@ -95,7 +95,7 @@ export default function LoginPage() {
           email: string;
           nickname: string;
           phone_number: string;
-          provider: string;
+          provider: number;
         };
       };
 
@@ -110,45 +110,6 @@ export default function LoginPage() {
       setStatus((prev) => ({ ...prev, login: 'error' }));
     }
   };
-
-  const handleSocialLogin = (provider: 'google' | 'kakao') => {
-    window.location.href = `/api/auth/${provider}/redirect`;
-  };
-
-  const login = useGoogleLogin({
-    onSuccess: async (tokenResponse) => {
-      try {
-        // res google api 주소에 headers로 토큰을 보낸다.
-
-        // 그리고 res.json() 해서 userInfo를 받는다.
-        const res = await fetch('https://openidconnect.googleapis.com/v1/userinfo', {
-          headers: {
-            Authorization: `Bearer ${tokenResponse}`,
-          },
-        });
-        console.log('res: ', res);
-
-        if (!res.ok) throw new Error(`HTTP ${res.status}`);
-        const data = await res.json();
-        console.log('data: ', data);
-
-        if (data.success) {
-          // 2) 로컬 스토리지에 액세스 토큰 저장
-          localStorage.setItem('access_token', data.accessToken);
-          // 3) 필요하면 전역 상태에 유저 정보 저장
-          // setUser(data.user); 등
-          router.push('/'); // 로그인 후 리다이렉트
-        } else {
-          console.error('서버 로그인 실패:', data.error);
-        }
-      } catch (err) {
-        console.error('구글 로그인 처리 중 에러:', err);
-      }
-    },
-    onError: () => {
-      console.error('구글 로그인 자체가 실패했습니다.');
-    },
-  });
 
   return (
     <div>
@@ -236,31 +197,8 @@ export default function LoginPage() {
         </div>
 
         <div className="flex flex-col justify-center gap-4">
-          {/* <button
-            onClick={() => handleSocialLogin('google')}
-            className="border-gray-3 flex justify-center gap-2 rounded-md border bg-white px-4 py-3 hover:cursor-pointer"
-          >
-            <Image
-              src="/images/social/google-logo.svg"
-              alt="구글 로그인 아이콘"
-              width={20}
-              height={24}
-            />
-            <div>구글로 로그인하기</div>
-          </button> */}
-          <button onClick={() => login()}>구글로 로그인</button>
-          <button
-            onClick={() => handleSocialLogin('kakao')}
-            className="flex justify-center gap-2 rounded-md bg-[#FEE500] px-4 py-3 hover:cursor-pointer"
-          >
-            <Image
-              src="/images/social/kakao-logo.svg"
-              alt="카카오 로그인 아이콘"
-              width={20}
-              height={24}
-            />
-            <div>카카오톡으로 로그인하기</div>
-          </button>
+          <GoogleLoginButton />
+          <KakaoLoginButton />
         </div>
       </div>
     </div>
