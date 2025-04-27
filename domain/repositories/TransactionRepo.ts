cdd0846006c87d636fb80c75cb9844811a375845
junik <wkdwnsdlr37@gmail.com>
2025-04-27 13:46:05 +0900

    Refactor: GETmonthlySummary interface 재정의

--- domain/repositories/TransactionRepo.ts
+++ domain/repositories/TransactionRepo.ts
@@ -1,9 +1,9 @@
 export interface TransactionRepo {
-  GETmonthlySummary(
-    userId: string,
-    startDate: string,
-    endDate: string
-  ): Promise<{ amount: number; is_expense: Boolean; date: string }[] | []>;
+  GETmonthlySummary(props: {
+    userId: string;
+    startDate: string;
+    endDate: string;
+  }): Promise<Array<{ amount: number; is_expense: Boolean; date: string }> | []>;
 
   GETdailySummary(
     userId: string,
