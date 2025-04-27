851abac744fbaf328ba64b1f6b8049696771a4e2
junik <wkdwnsdlr37@gmail.com>
2025-04-27 13:44:01 +0900

    Feat: periodBarChartContainer 예외처리 적용

--- app/components/user/container/PeriodBarChartContainer.tsx
+++ app/components/user/container/PeriodBarChartContainer.tsx
@@ -1,19 +1,63 @@
-import { type Period } from '@/app/shared/types/Chart';
-import PeriodBarChart from '@/app/components/user/presentation/PeriodBarChart';
+'use client'
+// package
+import { useEffect, useState, ReactElement, memo } from 'react';
+import { useRouter } from 'next/navigation';
+// slice
+import PeriodBarChart from '../presentation/PeriodBarChart';
 
 
-const bardata: Period[] = [
-  { name: '2024년 1월', income: 3000000, expense: 1200000 },
-  { name: '2024년 2월', income: 3300000, expense: 1000000 },
-  { name: '2025년 3월', income: 3200000, expense: 1800000 },
-  { name: '2025년 4월', income: 4000000, expense: 1500000 },
-  { name: '2025년 5월', income: 0, expense: 0 },
-  { name: '2025년 6월', income: 0, expense: 0 },
-  { name: '2025년 7월', income: 0, expense: 0},
-];
-
-export default function PeriodBarChartContainer() {
-    return(
-        <PeriodBarChart periodList={bardata} />
-    );
+type PeriodBarChartContainerProps = {
+  years: number;
 }
+
+export type PeriodItem = {
+  date: string;
+  expenses: number;
+  incomes: number;
+}
+
+type FetchData = {
+  status: number;
+  data: PeriodItem[] | null;
+};
+
+export default memo(function PeriodBarChartContainer({years}: PeriodBarChartContainerProps):ReactElement  {
+  const router = useRouter()
+  const [fetchData, setFetchData] = useState<FetchData | null>(null);
+  const [monthlyList, setMonthlyList] = useState<PeriodItem[]>([]);
+
+  useEffect(() => {
+      (async () => {
+        const res = await fetch(`/api/transactions/monthly?startDate=${years}-01&endDate=${years}-12`, {
+          headers: { Authorization: `Bearer ${localStorage.getItem('access_token')}` },
+        });
+  
+        const response = await res.json();
+        setFetchData(response);
+      })();
+    }, [years]);
+
+    useEffect(() => {
+      if (!fetchData) return;
+  
+      switch (fetchData.status) {
+        case 200:
+          if (!fetchData.data) return;
+          setMonthlyList(fetchData.data);
+          break;
+        case 401:
+          // 임시
+          alert("회원가입후 이용가능합니다");
+          router.push('/login');
+          break;
+        case 500:
+          // 임시: 500페이지 필요
+          router.push('/500');
+          break;
+      }
+    }, [fetchData]);
+
+    return(
+        <PeriodBarChart periodList={monthlyList} />
+    );
+})
