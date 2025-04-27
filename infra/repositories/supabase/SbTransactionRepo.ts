0d95cbbb6ec20a07f1352104a9d0812eac30e145
junik <wkdwnsdlr37@gmail.com>
2025-04-27 13:45:43 +0900

    Refactor: GETmonthlySummary 타입정의 및 try,catch 제거

--- infra/repositories/supabase/SbTransactionRepo.ts
+++ infra/repositories/supabase/SbTransactionRepo.ts
@@ -1,25 +1,26 @@
 import { TransactionRepo } from '@/domain/repositories/TransactionRepo';
 import { supabase } from '@/app/shared/lib/supabase';
 
-export class SbTransactionRepo implements TransactionRepo {
-  async GETmonthlySummary(
-    userId: string,
-    startDate: string,
-    endDate: string
-  ): Promise<{ amount: number; is_expense: Boolean; date: string }[] | []> {
-    try {
-      const { data, error } = await supabase
-        .from('transaction')
-        .select(`amount, is_expense, date`)
-        .eq('user_id', userId)
-        .gte('date', startDate)
-        .lte('date', endDate);
+type GETmonthlySummaryProps = {
+  userId: string;
+  startDate: string;
+  endDate: string;
+};
 
-      return data ? data : [];
-    } catch (error) {
-      console.error('error:', error);
-      return [];
-    }
+export class SbTransactionRepo implements TransactionRepo {
+  async GETmonthlySummary(props: GETmonthlySummaryProps): Promise<Array<{ amount: number; is_expense: Boolean; date: string }> | []> {
+    const { userId, startDate, endDate } = props;
+    const { data, error } = await supabase
+      .from('transaction')
+      .select(`amount, is_expense, date`)
+      .eq('user_id', userId)
+      .gte('date', startDate)
+      .lte('date', endDate)
+      .order('date', { ascending: true });
+
+    if(error) throw new Error(error.message);
+
+    return data ?? [];
   }
 
   async GETdailySummary(
